d0d0aeaca702160e7d770a487019601c5b168ce4
embedsri <chsriram@gmail.com>
2019-06-16 08:43:05 -0400

    Initial version

---
+++ YellowcircleDetect.h
@@ -0,0 +1,16 @@
+/*
+ * YellowcircleDirect.h
+ *
+ *  Created on: Jun 13, 2019
+ *      Author: bangaru
+ */
+
+#ifndef YELLOWCIRCLEDETECT_H_
+#define YELLOWCIRCLEDETECT_H_
+
+#include "opencv2/highgui/highgui.hpp" //Had to install libopencv-highgui-dev
+#include "opencv2/imgproc/imgproc.hpp" //Had to install libopencv-imgproc-dev
+#include <iostream>
+#include <vector>
+
+#endif /* YELLOWCIRCLEDIRECT_H_ */
